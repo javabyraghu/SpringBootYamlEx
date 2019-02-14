15d7f72a60e682b16ab4f8367c60581164d4abe7
javabyraghu <javabyraghu@gmail.com>
2019-02-14 23:04:56 +0530

    First Commit

---
+++ src/main/java/org/sathyatech/raghu/SpringBootYamlExApplication.java
@@ -0,0 +1,14 @@
+package org.sathyatech.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootYamlExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootYamlExApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/sathyatech/raghu/SpringBootYamlExApplicationTests.java
@@ -0,0 +1,17 @@
+package org.sathyatech.raghu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootYamlExApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
